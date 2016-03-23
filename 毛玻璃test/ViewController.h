1d98d73b933a1a841f23d0fa5ee554bec5a9a897
DongJin9557 <951891012@qq.com>
2016-03-23 10:10:07 +0800

    Initial Commit

---
+++ 毛玻璃test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  毛玻璃test
+//
+//  Created by dj on 16/3/23.
+//  Copyright © 2016年 dj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 毛玻璃test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  毛玻璃test
+//
+//  Created by dj on 16/3/23.
+//  Copyright © 2016年 dj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
